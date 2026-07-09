1897f11b015b94d5eaaedbf5c4de255400d97792
Eroroshy <eroroshy666@gmail.com>
2026-07-09 18:51:20 -0500

    chore: clean up main.ts by removing unnecessary whitespace

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createHead } from '@vueuse/head'
-
 import './style.css'
 import App from './App.vue'
-
 const app = createApp(App)
-
 app.use(createPinia())
-
 const head = createHead()
 app.use(head)
-
 app.mount('#app')
